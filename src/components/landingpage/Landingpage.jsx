13c3484a053b7de7de363ce61e9734ff39124808
Ena M <69399402+enamanjgo@users.noreply.github.com>
2022-07-14 01:09:08 -0400

    WIP message

--- src/components/landingpage/Landingpage.jsx
+++ src/components/landingpage/Landingpage.jsx
@@ -13,6 +13,7 @@ export default class Landingpage extends Component {
               <br />
               <br />
               <br />
+              <h4>Website in progress!</h4>
               <h4 id="title" class="title overflow-hidden fw-light text-light">
                 Nice to meet you! I'm{' '}
               </h4>
